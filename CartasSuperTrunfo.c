fcb801ca6f13e6a3aa31380eb3acfa9060d50fa8
FELIPE FRANCA DE ALMEIDA <97770128+dev-lipe@users.noreply.github.com>
2025-05-02 17:03:44 +0000

    adcionado 2 novos itens, pib e densidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,8 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int pontosturisticos1, pontosturisticos2;
+    float densidade1, densidade2;
+    float pibpc1, pibpc2;
 
     //coletando dados da primeira carta
     printf("Escolha uma letra entre 'A' a 'H' para representar seu estado: \n");
@@ -65,19 +67,31 @@ int main() {
    printf("Código: %s\n", codcarta1);
    printf("Nome da Cidade: %s\n", cidade1);
    printf("População: %lf habitantes\n", populacao1);
-   printf("Área: %f km²\n", area1);
-   printf("PIB: R$ %f bilhões de reais\n", pib1);
+   printf("Área: %.2f km²\n", area1);
+   printf("PIB: R$ %.2f bilhões de reais\n", pib1);
    printf("Número de Pontos Turísticos: %d\n", pontosturisticos1);
+   
+   densidade1 = populacao1 / (area1 + (area1 == 0));
+   printf("Densidade populacional: %.2lf por km²\n", densidade1);
+   
+   pibpc1 = (pib1 * 1000000000.0) / (populacao1 + (populacao1 == 0));
+   printf("PIB per capita: %.2lf reais\n", pibpc1);
+
 
    printf("\n====== CARTA 2 ======\n");
    printf("Estado: %c\n", estado2);
    printf("Código: %s\n", codcarta2);
    printf("Nome da Cidade: %s\n", cidade2);
-   printf("População: %lf habitantes\n", populacao2);
-   printf("Área: %f km²\n", area2);
-   printf("PIB: R$ %f bilhões de reais\n", pib2);
+   printf("População: %f habitantes\n", populacao2);
+   printf("Área: %.2f km²\n", area2);
+   printf("PIB: R$ %.2f bilhões de reais\n", pib2);
    printf("Número de Pontos Turísticos: %d\n", pontosturisticos2);
+   
+   densidade2 = populacao2 / (area2 + (area2 == 0));
+   printf("Densidade populacional: %.2lf por km²\n", densidade2);
 
+   pibpc2 = (pib2 * 1000000000.0) / (populacao2 + (populacao2 == 0));
+   printf("PIB per capita: %.2lf reais\n", pibpc2);
 
 
    return 0;
